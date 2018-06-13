1516a18ddec63893530a48d3e8e490c236429466
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-13 22:36:08 +0000

    Picture feature fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^register$', register, name='Register'),
     url(r'^logout$',   logout,   name='Logout'),
     url(r'^profile$',  profile,  name='Profile'),
-    url(r'^password-reset/$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
-    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/$',           password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
+    url(r'^password-reset/done/$',      password_reset_done, name='password_reset_done'),
     url(r'^password-reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete')
+    url(r'^password-reset/complete/$',  password_reset_complete, name='password_reset_complete')
 ]
